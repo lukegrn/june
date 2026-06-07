2904e914259188ee50a133603166b4e5a7957ddb
Luke Green <me@lukegreen.xyz>
2026-06-07 17:32:47 -0500

    Read raw file from test dir and print camera information

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use rsraw::RawImage;
+
 fn main() {
-    println!("Hello, world!");
+    let f = std::fs::read("raws/test.RAF").unwrap();
+    let raw = RawImage::open(&f).unwrap();
+    let info = raw.full_info();
+    println!("Camera: {} {}", info.make, info.model);
 }
